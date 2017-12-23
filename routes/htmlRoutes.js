d0d7ede1bc13696f6d15cc49041da6769b87402c
Laura Unaeze <l.unaeze5@gmail.com>
2017-12-22 19:15:36 -0700

    Added new files/routes. Changed a couple minor thigns for the appearance.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,8 +11,12 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("/skills", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/skills.html"));
+  app.get("/projects", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/projects.html"));
+  });
+
+  app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/contact.html"));
   });
 
   // If no matching route is found default to home
