6da11b19716347e33087e9e042a19741d52afc52
Laura Unaeze <l.unaeze5@gmail.com>
2017-12-28 14:48:12 -0700

    Changed skills list to table. Changed 'project' to 'portfolio' and all related routes/links.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,8 +11,8 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("/projects", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/projects.html"));
+  app.get("/portfolio", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/portfolio.html"));
   });
 
   app.get("/contact", function(req, res) {
